7278044a9ab8e303be0180c9f53f77dd2d736dce
atikrangnekar28@gmail.com <atikrangnekar28@gmail.com>
2023-03-08 10:33:37 +0530

    adding cart and shopping icon as well as adding shopping list ie cart

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.scss';
 import App from './App';
-import {UserProvider} from './contexts/user.context'
+import { UserProvider } from './contexts/user.context'
 import { BrowserRouter } from 'react-router-dom';
 import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
+
 
 const rootElement = document.getElementById('root');
 render(
   <React.StrictMode>
     <BrowserRouter>
-    <UserProvider>
-    <ProductsProvider>
-          <App />
-    </ProductsProvider>
-     
-    </UserProvider>
+      <UserProvider>
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
+
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   rootElement
