6c4ceefb91e4bee98d42ca033b636984b316bf32
atikrangnekar28@gmail.com <atikrangnekar28@gmail.com>
2023-03-13 12:28:56 +0530

    Done with checkout page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { useContext } from 'react';
-import { CartContext } from '../../contexts/cart.context';
-
-import './checkout-item.styles.scss'
-const CheckoutItem = ({ cartItem }) => {
-    const { name, imageUrl, price, quantity } = cartItem;
-    const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(CartContext);
-   
-    const clearItemHandler = () => clearItemFromCart(cartItem);
-    const addItemHandler = ()=> addItemToCart(cartItem);
-    const removeItemHandler = ()=> removeItemFromCart(cartItem);
-    
-=======
 import './checkout-item.styles.scss'
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
@@ -27,7 +13,7 @@ const CheckoutItem = ({ cartItem }) => {
     const removeItemHandler = () => removeItemToCart(cartItem);
 
 
->>>>>>> 0d08e4635118c284fcf37de181b66e5cbc23379b
+
     return (
         <div className='checkout-item-container'>
             <div className='image-container'>
@@ -35,35 +21,21 @@ const CheckoutItem = ({ cartItem }) => {
             </div>
             <span className='name'>{name}</span>
             <span className='quantity'>
-<<<<<<< HEAD
+
                 <div className='arrow' onClick = {removeItemHandler} >
                     &#10094;
                 </div>
                 <span className='value'>{quantity}</span>
-=======
-                <div className='arrow' onClick={removeItemHandler}>
-                    &#10094;
-                </div>
-                <span className='value'>
-                    {quantity}
-                </span>
->>>>>>> 0d08e4635118c284fcf37de181b66e5cbc23379b
                 <div className='arrow' onClick={addItemHandler}>
                     &#10095;
                 </div>
             </span>
             <span className='price'>{price}</span>
             <span className='remove-button' onClick={clearItemHandler}>&#10005;</span>
-<<<<<<< HEAD
+
         </div>
 
     )
 }
 
-=======
-
-        </div>
-    )
-}
->>>>>>> 0d08e4635118c284fcf37de181b66e5cbc23379b
 export default CheckoutItem;
